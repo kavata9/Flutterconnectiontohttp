9b32d2937dc9e469e552f123407b337087670070
Agnes Kavata Musyoka <kavatamusyoka24@gmail.com>
2019-05-30 18:47:41 +0300

    add url t fetch data using api call

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+
 void main() => runApp(new MaterialApp(
   home: new HomePage(),
 ));
@@ -22,7 +25,20 @@ void initState() {
   }
 
   Future<String>getJsonData() async{
-  var response = await http
+  var response = await http.get(
+    //encode the url
+    Uri.encodeFull(url),
+    //accept only json response
+    headers: {"Accept": "application/json"}
+  );
+
+  print(response.body);
+
+  setState(() {
+    var convertDataToJson = json.decode(response.body);
+    data = convertDataToJson["results"];
+  });
+  return "Success";
 
   }
 
@@ -36,7 +52,7 @@ void initState() {
 
     ),
     body: new ListView.builder(
-      itemCount: 1,
+      itemCount: data == null ? 0 : data.length,
       itemBuilder: (BuildContext context,int index){
         return new Container(
           child: new Center(
@@ -45,7 +61,7 @@ void initState() {
               children: <Widget>[
                 new Card(
                   child: new Container(
-                    child: Text("Hello"),
+                    child: Text(data[index]['name']),
                     padding: const EdgeInsets.all(20.0),
 
                   ),
